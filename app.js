0e0db8609ee71cb9f54ac4845ba83f829547802d
deniaverkade <9024406@student.zadkine.nl>
2024-10-21 16:04:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var budget = 100
+budget = 100
+let product = prompt("Hoeveel kost het product?");
 
-var product = 60 
-
-function budgetter(){
-    if (product <= budget){
-        document.getElementById("tekst").innerHTML="U heeft genoeg geld!";
-        document.getElementById("tekst").style.color="green";
-    }
-    else {
-        document.getElementById("tekst").innerHTML="Helaas, te weinig geld.";
-        document.getElementById("tekst").style.color="red";
-    }
+function budgetter() {
+    if (product <= budget) {
+document.getElementById("tekst").innerHTML="U heeft genoeg geld!"
+document.getElementById("tekst").style.color="green"
+}
+else {
+document.getElementById("tekst").innerHTML="Helaas, u heeft niet genoeg geld."
+document.getElementById("tekst").style.color="red"
+}
 }
